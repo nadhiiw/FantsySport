e006d772a86272b96b8578344ce3d2d3b8de4a99
Nadhi Woliye <nwoliye@gmail.com>
2022-11-17 21:54:44 -0600

    create a different packages

--- src/main/java/com/project/FantasySport/FantasySportApplication.java
+++ src/main/java/com/project/FantasySport/FantasySportApplication.java
@@ -2,12 +2,19 @@ package com.project.FantasySport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.awt.*;
 
 @SpringBootApplication
+
 public class FantasySportApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(FantasySportApplication.class, args);
 	}
+
 
 }

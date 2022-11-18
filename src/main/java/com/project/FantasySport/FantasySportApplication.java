a8cd9e1f41908839161bd2a35ff353080f83379c
Nadhi Woliye <nwoliye@gmail.com>
2022-11-17 21:30:03 -0600

    create the spring boot

---
+++ src/main/java/com/project/FantasySport/FantasySportApplication.java
@@ -0,0 +1,13 @@
+package com.project.FantasySport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasySportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasySportApplication.class, args);
+	}
+
+}

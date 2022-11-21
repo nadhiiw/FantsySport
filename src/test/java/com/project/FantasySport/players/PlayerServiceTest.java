4fa2349b80118fe1d8d8011bfb903fc10c3e31ad
Nadhi Woliye <nwoliye@gmail.com>
2022-11-21 10:48:29 -0600

    create class for the player Service testing

---
+++ src/test/java/com/project/FantasySport/players/PlayerServiceTest.java
@@ -0,0 +1,28 @@
+package com.project.FantasySport.players;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerServiceTest {
+
+    @Test
+    void addPlayer() {
+    }
+
+    @Test
+    void getAllPlayer() {
+    }
+
+    @Test
+    void getPlayerById() {
+    }
+
+    @Test
+    void deletePlayer() {
+    }
+
+    @Test
+    void updatePlayer() {
+    }
+}
